cb5caf4feb77e92ca194285f848d9071f0df83f7
Nicegic <registrierenadresse@t-online.de>
2015-11-21 17:20:39 +0100

    Package-Struktur erstellt, Import des Projektes in JMonkeyEngine3.0

---
+++ src/generation/NoiseFunction.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generation;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class NoiseFunction {
+
+    public Ausgabe generate(double x, double y) {
+
+    }
+}
