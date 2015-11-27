b40c70c4fd7ae71aa46132d51935d2fb1a65b78d
Colin <Colin@Colin-PC04.fritz.box>
2015-11-27 15:46:23 +0100

    Primitive Logik zum Erzeugen von Chunks (Testzweck)

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -6,6 +6,7 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 
 /**
@@ -21,14 +22,54 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
+        Box b = new Box(1, 1, 1); // create cube shape
+        
+        
+        // Definiere 3 Blockmaterialien
+        Material red = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        red.setColor("Color", ColorRGBA.Red);
+        
+        Material blue = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        blue.setColor("Color", ColorRGBA.Green);
+        
+        Material yellow = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        blue.setColor("Color", ColorRGBA.Yellow);
+        
+        // Zur Besseren Testansicht: Startpunkt unter 0
+        int start = -50;
+        
+        Node pivot = new Node("pivot");
+        
+        // Unschön gemacht, nur zum Testen wie man die Objekte handhabt so gemacht
+        Geometry geom[][][] = new Geometry[16][16][16];
+        
+        for (int i = 0; i < 16; i++){
+            for (int j = 0; j < 16; j++){
+                
+                int rand = ((int)(Math.random() * 14)) + 3;
+                
+                for (int k = 0; k < rand; k++){
+                    geom[i][j][k] = new Geometry ("Box", b);
+                    geom[i][j][k].setLocalTranslation(new Vector3f(i*2,(start + 2 * k), (-100 + 2 * j)));
+                    
+                    if (k < 3){
+                        geom[i][j][k].setMaterial(blue);        // Irgendwie verhaut er bei 3 Farben eine Farbe
+                    }
+                    else if (k == rand-1){
+                        geom[i][j][k].setMaterial(yellow);
+                    }
+                    else {
+                        geom[i][j][k].setMaterial(red);
+                    }
+  
+                    pivot.attachChild(geom[i][j][k]); 
+                }                    
+            }
+          }
+        
+       
+        rootNode.attachChild(pivot);              // make the cube appear in the scene
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
     }
 
     @Override
@@ -41,3 +82,4 @@ public class Main extends SimpleApplication {
         //TODO: add render code
     }
 }
+
