25d0d0b68b7a350675276fb0c5a633d8f7891460
Nicegic <registrierenadresse@t-online.de>
2015-11-28 17:06:06 +0100

    Pseudogenerierung mit Simplex-Noise

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -8,6 +8,7 @@ import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
+import junit.generation.OpenSimplexNoise;
 
 /**
  * test
@@ -23,6 +24,7 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleInitApp() {
         Box b = new Box(1, 1, 1); // create cube shape
+        OpenSimplexNoise osn = new OpenSimplexNoise();
         
         
         // Definiere 3 Blockmaterialien
@@ -30,33 +32,36 @@ public class Main extends SimpleApplication {
         red.setColor("Color", ColorRGBA.Red);
         
         Material blue = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        blue.setColor("Color", ColorRGBA.Green);
+        blue.setColor("Color", ColorRGBA.Blue);
         
         Material yellow = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        blue.setColor("Color", ColorRGBA.Yellow);
+        yellow.setColor("Color", ColorRGBA.Yellow);
         
         // Zur Besseren Testansicht: Startpunkt unter 0
         int start = -50;
         
         Node pivot = new Node("pivot");
+        int x,y,z;
+        x=y=32;       //NUR FÜR TESTZWECKE
+        z=16;
         
         // Unschön gemacht, nur zum Testen wie man die Objekte handhabt so gemacht
-        Geometry geom[][][] = new Geometry[16][16][16];
+        Geometry geom[][][] = new Geometry[x][y][z];
         
-        for (int i = 0; i < 16; i++){
-            for (int j = 0; j < 16; j++){
+        for (int i = 0; i < x; i++){
+            for (int j = 0; j < y; j++){
                 
-                int rand = ((int)(Math.random() * 14)) + 3;
+                int rand = (int)(osn.eval(i/10.0, j/10.0)*5+10); //max Höhe einer Säule mit SimplexNoise bestimmen
                 
                 for (int k = 0; k < rand; k++){
                     geom[i][j][k] = new Geometry ("Box", b);
                     geom[i][j][k].setLocalTranslation(new Vector3f(i*2,(start + 2 * k), (-100 + 2 * j)));
                     
-                    if (k < 3){
-                        geom[i][j][k].setMaterial(blue);        // Irgendwie verhaut er bei 3 Farben eine Farbe
+                    if (k < 2){
+                        geom[i][j][k].setMaterial(yellow);        // Irgendwie verhaut er bei 3 Farben eine Farbe
                     }
                     else if (k == rand-1){
-                        geom[i][j][k].setMaterial(yellow);
+                        geom[i][j][k].setMaterial(blue);
                     }
                     else {
                         geom[i][j][k].setMaterial(red);
